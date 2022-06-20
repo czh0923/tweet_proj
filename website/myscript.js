c84b192d1a87cd08888c5c2c2947978c9a4241f2
czh0923 <zchenggn@126.com>
2022-06-19 23:18:07 -0700

    clicking button to insert data workinggit add .!

--- website/myscript.js
+++ website/myscript.js
@@ -8,4 +8,36 @@ const secondTweet = "2. the second tweet";
 
 document.getElementById("1st").innerHTML = firstTweet;
 document.getElementById("2nd").innerHTML = secondTweet;
-document.getElementById("submitButton").addEventListener("click", f);
+
+
+
+
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyQUcYkOoAkjWXTV'}).base('app0HYDUVfrBVLFV2');
+
+console.log("here");
+
+const table = base('ex_tweets');
+
+data1 = {
+    "Name": "czh",
+    "content": "hello!\n",
+    "visited": 0
+    
+};
+
+function f_create(err, records) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    records.forEach(function (record) {
+      console.log(record.getId());
+    })
+};
+
+function insert() {
+    console.log("here");
+    table.create(data1, f_create);
+}
+document.getElementById("submitButton").addEventListener("click", insert);
